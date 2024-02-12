0a6fe1868b520231886a51c919d22bfc0ef816bb
Sedanur <sedanurinan@gmail.com>
2024-02-12 14:41:00 +0300

    emit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <img src="@/assets/kedii.jpeg" style="width: 50%; height: 100%" />
+      <p>Cats</p>
+    </div>
+
+    <div>
+      <button @click="addCat">Ekle</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { catCount: 0 };
+  },
+  methods: {
+    addCat() {
+      this.$emit("addCats", this.catCount);
+    },
+  },
+};
+</script>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,32 @@
+<template>
+ <ChildComponent @addCats="addCats"></ChildComponent> 
+  <div>
+    Sepet: {{ countOfItem }}
+  </div>
+</template>
+<script>
+import ChildComponent from './ChildComponent.vue';
+export default(
+{
+    data() {
+        return {
+            countOfItem:0
+        }
+    },
+    components: {
+        ChildComponent
+    },
+    methods: {
+       addCats() {
+        this.countOfItem += 1
+       }
+    }
+
+})
+</script>
+<style>
+.clickedStyle{
+    color:pink;
+    background-color: aqua;
+}
+</style>
